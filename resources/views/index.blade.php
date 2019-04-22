95c3fb38104f5f3164500177d11d603181e0648c
unknown <danepainaga@gmail.com>
2019-04-22 11:25:46 +0800

    Updated PPMS Code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,85 +6,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>PPMS</title>
-
-        <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
         <link href="{{asset('node_modules/angularjs-datepicker/dist/angular-datepicker.min.css')}}" rel="stylesheet" type="text/css">
         <link href="{{asset('node_modules/angular-confirm/dist/angular-confirm.min.css')}}" rel="stylesheet" type="text/css">
         <link href="{{asset('node_modules/components-font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{asset('bower_components/fullcalendar/dist/fullcalendar.min.css')}}"/>
-        <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar-scheduler/1.9.4/scheduler.css"> -->
-        <!-- <link rel="stylesheet" href="{{asset('node_modules/placeholder-loading/dist/css/placeholder-loading.min.css')}}"/> -->
-        <!-- <link rel="stylesheet" href="https://unpkg.com/placeholder-loading/dist/css/placeholder-loading.min.css"> -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body ng-cloak ng-controller="mainAppCtrl">
-    <ng-include src="'views/header.html'" ></ng-include> 
-    <div class="container-fluid ">
-        <div class="row" ui-view=""></div>
-
+        <ng-include src="'views/header.html'" ></ng-include> 
+        <div class="container-fluid ">
+            <div class="row" ui-view=""></div>
+        </div>
+        <ng-include src="'views/modal.html'" ng-if="header"></ng-include> 
 
         <script src="{{asset('node_modules/jquery/dist/jquery.min.js')}}" type="text/javascript"></script>
-
         <script type="text/javascript" src="{{asset('bower_components/moment/min/moment.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('bower_components/fullcalendar/dist/fullcalendar.min.js')}}"></script>
-        <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar-scheduler/1.9.4/scheduler.min.js"></script> -->
         <script type="text/javascript" src="{{asset('bower_components/fullcalendar/dist/gcal.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
         <script type="text/javascript" src="{{asset('bower_components/angular-ui-calendar/src/calendar.js')}}"></script>
@@ -100,9 +37,6 @@
         <script src="{{asset('node_modules/sweetalert2/dist/sweetalert2.min.js')}}" type="text/javascript"></script>
 
         <script>var baseUrl = "{{url('/')}}/";</script>
-
-    </div>
-        <ng-include src="'views/modal.html'" ng-if="header"></ng-include> 
         <script type="text/javascript" src="{{asset('controller/mainCtrl.js')}}"></script>
         <script type="text/javascript" src="{{asset('services/allServices.js')}}"></script>
         <script type="text/javascript" src="{{asset('alertServices/alertServices.js')}}"></script>
